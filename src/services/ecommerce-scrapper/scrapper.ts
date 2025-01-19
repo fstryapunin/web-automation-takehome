0348940f4c206a3d0f051b882f3c24a336c7a024
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-01-19 17:05:13 +0100

    added comments

--- src/services/ecommerce-scrapper/scrapper.ts
+++ src/services/ecommerce-scrapper/scrapper.ts
@@ -18,7 +18,10 @@ export class EcommerceScrapper {
   }
 
   public async scrapeProducts(priceDistribution: ExpectedPriceDistribution) {
+    // Get total product count from api (equal to total for maximum price range).
     const totalProductCount = await this.getTotalProductCount();
+
+    // Get function to calculate expected price window for a 1000 products. See utils.ts.
     const calculatePriceWindowSize =
       WindowSizeCalculatorByDistributionType[priceDistribution];
 
@@ -30,15 +33,18 @@ export class EcommerceScrapper {
       leftPriceBound < Configuration.PRODUCT_MAX_PRICE &&
       products.length < totalProductCount
     ) {
+      // Calculate right bound of the price window expected to return about a 1000 products.
       const rightPriceBound =
         leftPriceBound +
         calculatePriceWindowSize(totalProductCount, leftPriceBound, retries);
 
+      // This is expected to fetch slightly less than a thousand products.
       const nextProductBatch = await this._apiClient.getProducts({
         minPrice: leftPriceBound,
         maxPrice: rightPriceBound,
       });
 
+      // If too much products were fetched, discard and try again with a smaller window.
       if (
         nextProductBatch.total > Configuration.MAX_PRODUCTS_RETURNED_PER_REQUEST
       ) {
@@ -46,7 +52,9 @@ export class EcommerceScrapper {
         continue;
       }
 
+      // Shift the left bound past already fetched products.
       leftPriceBound = leftPriceBound + rightPriceBound;
+
       products.concat(nextProductBatch.products);
       console.log(
         `Succesfully scrapped ${products.length} out of ${totalProductCount}`
