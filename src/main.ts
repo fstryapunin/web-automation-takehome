3a215301b422eba61017fb91be3de766d6d25847
Fedor Stryapunin <fedor.stryapunin@gmail.com>
2025-01-19 16:18:46 +0100

    added running in the scrapper in main

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,13 @@
 import "dotenv/config";
+import { Configuration } from "./configuration/configuration.ts";
+import { EcommerceScrapper } from "./services/ecommerce-scrapper/scrapper.ts";
+import { ExpectedPriceDistribution } from "./services/ecommerce-scrapper/utils.ts";
 
 Configuration.validateConfiguration();
+
+const scrapper = new EcommerceScrapper();
+const products = await scrapper.scrapeProducts(ExpectedPriceDistribution.Equal);
+
+console.log(
+  `Finished scrapping. Scrapped a total of ${products.length} products.`
+);
